08565d89669b33eb453634c45c1ab04d31941648
apittendrigh <alex@chasesoftware.co.za>
2015-12-19 00:51:21 +0200

    Since the sample size is 8192 and this code returns 8193 as the number of home visited i am going to assume that there's a bug in teh quality operator.
    Signed-off-by: apittendrigh <alex@chasesoftware.co.za>

--- Day3/housevisitrecorder.js
+++ Day3/housevisitrecorder.js
@@ -1,5 +1,21 @@
-var HouseVisitRecorder = function () {
+var HouseVisitRecorder = function () {	
+	this.homesVisited = [];
+	this.isHomeInCollection = function (homeLocation) {
+		var found = false;
+		var i = 0;
+		while (!found && i < this.homesVisited.length) {
+			var currentHome = this.homesVisited[i++];
+			found = currentHome.equals(homeLocation);
+		};
+	}
+
 	this.notify = function (state) {
-		console.log(state);
+		if (!this.isHomeInCollection(state)){
+			this.homesVisited.push(new House(state.north,state.east,state.south,state.west));
+		};
+	}
+	
+	this.getHomesVisited = function () {
+		return this.homesVisited.length;
 	}
 }
