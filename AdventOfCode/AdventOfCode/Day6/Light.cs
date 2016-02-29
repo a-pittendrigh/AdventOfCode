d70ff2045f31e9f817205feeb15ca3186ecb3d3b
Alexander Pittendrigh <a.pittendrigh@gmail.com>
2016-02-29 20:13:12 +0200

    Started Day6 code. Wrote some objects that I think will be able to do what is requruired. Now I need to write the thing that looks at the intructions and look at all the tets.

---
+++ AdventOfCode/AdventOfCode/Day6/Light.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AdventOfCode.Annotations;
+
+namespace AdventOfCode.Day6
+{
+    public class Light : INotifyPropertyChanged
+    {
+        public Light()
+        {
+            InitFields();
+        }
+
+        private void InitFields()
+        {
+            IsOn = false;
+        }
+
+        private bool _isOn;
+
+        public bool IsOn
+        {
+            get { return _isOn; }
+            set
+            {
+                _isOn = value;
+                NotifyPropertyChanged(nameof(IsOn));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ AdventOfCode/AdventOfCode/Day6/LightDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Day6
+{
+    public class LightDisplay
+    {
+        #region Fields
+
+        private Light[,] _lightDisplay;
+        //It's basically a graph with only positive values;
+        private int domain; //This is the size of the x-axis, going form 0 - domain
+        private int range; //This is the size of the y-axis, going from 0 - range
+        #endregion
+        public LightDisplay(int domain, int range)
+        {
+            //This must be fixed,
+            _lightDisplay = new Light[range, domain];
+            for (int y = 0; y < range; y++)
+            {
+                for (int x = 0; x <= domain; x++)
+                {
+                    Light newLight = new Light();
+                    _lightDisplay[y, x] = newLight;
+                    newLight.PropertyChanged += (PropertyChangedHandler);
+                }
+            }
+            LightsLit = 0;
+        }
+
+        public int LightsLit { get; set; }
+
+        public void PropertyChangedHandler(object sender, PropertyChangedEventArgs e)
+        {
+            Light lightThatChanged = sender as Light;
+            if (lightThatChanged != null)
+            {
+                //Will have to change this if I need to implement other properties.
+                LightsLit++;
+            }
+        }
+    }
+}
