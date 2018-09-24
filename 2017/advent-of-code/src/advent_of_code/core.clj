0afcbc7e2b84b8af86c8ea9192206726fcba43b3
Alexander Pittendrigh <a.pittendrigh@gmail.com>
2018-09-24 09:21:49 +0200

    day1, part 2

--- 2017/advent-of-code/src/advent_of_code/core.clj
+++ 2017/advent-of-code/src/advent_of_code/core.clj
@@ -3,8 +3,7 @@
    [advent-of-code.day1 :as day1])
   (:gen-class))
 
-(defn -main
-  [& args]
-
+(defn -main [& args]
   (def day-1-input (slurp "resources/day1-input.txt"))
-  (println "Day 1: " (day1/sum-of-captcha day-1-input)))
+  (println "Day 1, Part 1: " (day1/sum-of-captcha (clojure.string/trim-newline day-1-input) 1))
+  (println "Day 1, Part 2: " (day1/sum-of-captcha (clojure.string/trim-newline day-1-input) (/ (count day-1-input) 2))))
--- 2017/advent-of-code/src/advent_of_code/day1.clj
+++ 2017/advent-of-code/src/advent_of_code/day1.clj
@@ -1,23 +1,34 @@
 (ns advent-of-code.day1)
 
+(defn get-char-n-away [position captcha n]
+  (def len (- (count captcha) 1))
+  (if (> (+ position n) len)
+    (nth captcha (- (+ position (- n 1)) len))
+    (nth captcha (+ position n))))
+
+
 (defn get-next-char [position captcha]
-  (if (= position (- (count captcha) 1))
-    (nth captcha 0)
-    (nth captcha (+ 1 position))))
+  (get-char-n-away position captcha 1))
+
+
+(defn matches-digit-n-away? [position captcha n]
+  (= (nth captcha position) (get-char-n-away position captcha n)))
 
 (defn matches-next-digit? [position captcha]
-  (= (nth captcha position) (get-next-char position captcha)))
+  (matches-digit-n-away? position captcha 1))
 
-(defn value-if-match [position captcha]
-  (if (matches-next-digit? position captcha)
+
+(defn value-if-matched-n-away [position captcha n]
+  (if (matches-digit-n-away? position captcha n)
     (Integer/parseInt (str (nth captcha position)))
     0))
 
-(defn sum-of-captcha [captcha]
+
+(defn sum-of-captcha [captcha n]
   (def letters (count captcha))
   (def max-position (- letters 1))
   (loop [sum 0 position 0]
-    (def new-sum (+ sum (value-if-match position captcha)))
+    (def new-sum (+ sum (value-if-matched-n-away position captcha n)))
     (if (>= position max-position)
       new-sum
       (recur
